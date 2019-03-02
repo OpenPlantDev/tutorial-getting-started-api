8a18c34725ef18500b0939c763e1a2fee62c3b7b
Daniel Nichols <danielrnichols63@gmail.com>
2019-03-01 20:00:38 -0600

    Added error handling

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import {ApiError} from "./ApiError";
 import {IApiRouter} from "./routers/IApiRouter";
 
 export class Api {
@@ -6,12 +7,24 @@ export class Api {
   public Start(routers: IApiRouter[]): void {
     const api = express();
 
+    // handle routes defined by routers
     for (const router of routers) {
       api.use(router.route, router.routeHandler());
     }
 
-    api.get("/api", (req, res) => {
-      return res.send("Hello from the API");
+    // handle error for non-handled routes
+    api.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+      // create new error with status 404
+      const err = new ApiError(404, `Route not found`);
+      next(err);
+    });
+
+    // handle errors thrown during the handling of the request
+    api.use((err: ApiError, req: express.Request, res: express.Response, next: express.NextFunction) => {
+      const status = err.status ? err.status : 500;
+      const message = err.message ? err.message : "Server error";
+
+      res.status(status).json({message});
 
     });
 
