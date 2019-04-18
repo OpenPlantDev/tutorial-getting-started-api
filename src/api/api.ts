0d6e862c4b4e1599a988d4a185522dfed0271412
Daniel Nichols <danielrnichols63@gmail.com>
2019-04-18 11:53:58 -0500

    Fixed CORS issue

--- src/api/api.ts
+++ src/api/api.ts
@@ -27,7 +27,7 @@ export class Api {
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
       if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "GET POST PUT DELETE");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         return res.status(200).json({});
       }
       return next();
